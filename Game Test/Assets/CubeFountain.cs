e5825fcd5a654dced34e012f433c08b31cd2267d
David Stewart <dstewart6@sae.au>
2017-06-14 11:42:34 +0800

    Update Test 01

---
+++ Game Test/Assets/CubeFountain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFountain : MonoBehaviour {
+	public GameObject cube;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject cube = GameObject.Instantiate (cube, transform.position, Quaternion.identity) as GameObject;
+
+		Rigidbody cubeRigidBody = cube.GetComponent<Rigidbody> ();
+
+		cubeRigidBody.AddForce (cube.transform.up * speed, ForceMode.VelocityChange);
+		
+	}
+}
